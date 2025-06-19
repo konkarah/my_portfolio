3c90204d708ed9ff10c01692e366c5f7bffaea83
konkarah <konkarahdeh@gmail.com>
2025-06-19 15:00:38 +0300

    initial commit

---
+++ src/data/education.ts
@@ -0,0 +1,37 @@
+import { type ExperienceShowcaseListItemProps } from "@/components/experience/experience-showcase-list-item";
+
+export const EDUCATION: ExperienceShowcaseListItemProps[] = [
+  {
+    title: "Bachelor Of Science In Computer Science",
+    organisation: {
+      name: "Masinde Muliro University of Science and Technology",
+      href: "https://www.mmust.ac.ke/",
+    },
+    date: "2017-2021",
+    location: "Kakamega, Kenya",
+    description:
+      "Relevant courses included Data Structures and Algorithms, Web Design, Cloud Computing, Artificial Neural Network and Database Management.",
+  },
+  {
+    title: "Certified Cloud Practitioner",
+    organisation: {
+      name: "AWS Restart Program",
+      href: "https://aws.amazon.com/training/restart/",
+    },
+    date: "Jan 2025 - April 2025",
+    location: "Remote",
+    description:
+      "Completed a comprehensive training program focused on cloud computing fundamentals, AWS services, and best practices. Gained hands-on experience with AWS tools and technologies, preparing for a career in cloud solutions.",
+  },
+  {
+    title: "Certified in Cybersecurity",
+    organisation: {
+      name: "ISC2",
+      href: "https://www.isc2.org/Certifications/Cybersecurity-Entry-Level",
+    },
+    date: "March 2025 - June 2025",
+    location: "Remote",
+    description:
+      "Completed a foundational course in cybersecurity, covering essential concepts such as network security, risk management, and incident response. Developed skills to identify and mitigate cybersecurity threats, preparing for a career in the field.",
+  },
+];
---
+++ src/data/experience.ts
@@ -0,0 +1,36 @@
+import { type ExperienceShowcaseListItemProps } from "@/components/experience/experience-showcase-list-item";
+
+export const EXPERIENCE: ExperienceShowcaseListItemProps[] = [
+  {
+    title: "Software Developer",
+    organisation: {
+      name: "Nathan & Nathan",
+      href: "https://nathandigital.com/",
+    },
+    date: "May 2024 - Present",
+    location: "Hybrid ",
+    description: "Spearheaded the development of an ERP suite leveraging AWS cloud infrastructure, resulting in a 10% increase in clientele, and was commended by the CTO for high standards.",
+  },
+  {
+    title: "Systems Engineer",
+    organisation: {
+      name: "Direcorate for Social Protection",
+      href: "https://socialprotection.go.ke/",
+    },
+    date: "May 2022 - May 2024",
+    location: "Onsite",
+    description:
+      "Conceptualized and effected a P2P/Blockchain solution for seamless payments to over 1.3 million cash transfer beneficiaries cutting disbursement times by 12 days and transfer levies by 30%.",
+  },
+  {
+    title: "Frontend Developer Intern",
+    organisation: {
+      name: "Dehmans Group",
+      href: "https://dehmans.com/",
+    },
+    date: "Feb 2019 - Nov 2021",
+    location: "Remote",
+    description:
+      "Conceived an unmanned email response system, which reduced response time to 3K email messages by 71%.",
+  },
+];
